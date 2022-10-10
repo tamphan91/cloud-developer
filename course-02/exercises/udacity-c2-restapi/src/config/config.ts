766d3879275fe8f9172be0106d407cb5acf47b37
tamphan91 <tamphan04101991@gmail.com>
2022-10-10 23:04:14 +0700

    implements TODO tasks, add dotenv

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,14 @@
+require("dotenv").config();
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": process.env.PROGRESS_USERNAME,
+    "password": process.env.PROGRESS_PASSWORD,
+    "database": process.env.PROGRESS_DATABASE,
+    "host": process.env.PROGRESS_HOST,
     "dialect": "postgres",
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
   },
   "jwt": {
     "secret": " "
